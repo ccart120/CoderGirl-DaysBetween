34afeaac3d76dd1294b7626e58b26733cfcac301
ccartagena879 <ccartagena879@gmail.com>
2019-03-12 23:21:32 -0500

    daysbetween commit

--- DayBetween/Program.cs
+++ DayBetween/Program.cs
@@ -7,10 +7,40 @@ namespace DaysBetween
         public static void Main()
         {
             // TODO:  Get two dates from the user.
+            Console.WriteLine("Enter a date: ");
+            string dateOne = Console.ReadLine();
+            DateTime dateBefore = DateTime.Parse(dateOne);
+
+            Console.WriteLine("Enter another date: ");
+            string dateTwo = Console.ReadLine();
+            DateTime dateAfter = DateTime.Parse(dateTwo);
+
+            
+
+            
+
+            Console.WriteLine(DaysBetween(dateAfter, dateBefore));
 
             Console.ReadLine();
         }
+        public static int DaysBetween(DateTime dateAfter, DateTime dateBefore )
+        {
+            TimeSpan datesBetween;
+            if (dateAfter > dateBefore)
+            {
+                datesBetween = dateAfter.Subtract(dateBefore);
+            }
 
+            else
+            {
+                datesBetween = dateBefore.Subtract(dateAfter);
+            }
+            
+            int daysBetween = datesBetween.Days;
+            return daysBetween;
+            
+
+        }
         // TODO: Create that has a method that counts the number of days between two different dates.
     }
 }
